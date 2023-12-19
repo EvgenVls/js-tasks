a5c2a90efd9ebed77849b314157426f0449c8ccd
EvgenVls <145988014+EvgenVls@users.noreply.github.com>
2023-12-19 16:02:10 +0200

    spread

---
+++ js/spread.js
@@ -0,0 +1,8 @@
+const first = { propA: 5, propB: 10, propC: 50 };
+const second = { propC: 15, propD: 20 };
+
+const third = { ...first, ...second };
+console.log(third);
+
+const fourth = { ...second, ...first };
+console.log(fourth);
